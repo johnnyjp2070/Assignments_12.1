8c7187669dd92c3aff15e38617c57d2997eadaeb
Johnson <johnnyjp2070@gmail.com>
2018-05-11 15:07:44 +0530

    Added text in the Console Output

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 function counterReducer(actionType, counter) {
     if(actionType === "INC"){
         counter += 1;
+        change = "Incremented"
     } else if (actionType === "DEC"){
         counter -= 1;
+        change = "Decremented"
     }
     return counter;
 }
 
 var result = counterReducer('DEC', 5);
-console.log(result);
+console.log(change+" - "+result);
